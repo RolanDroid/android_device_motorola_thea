2c7518de10e9fd4d48360098f7379d43ab3c7303
RolanDroid <rolandosimoncelli8@gmail.com>
2015-10-24 15:21:00 +0200

    Added bootanimation
    Signed-off-by: RolanDroid <rolandosimoncelli8@gmail.com>

--- eos.mk
+++ eos.mk
@@ -7,3 +7,7 @@ PRODUCT_RELEASE_NAME := thea
 PRODUCT_NAME := eos_thea
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    device/motorola/msm8226-common/configs/bootanimation.zip:system/media/bootanimation.zip
